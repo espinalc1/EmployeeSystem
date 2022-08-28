1dc9e8f3d0305806f383fee380e72a5c6c2b5c25
espinalc1 <christopher.espinal@gmail.com>
2022-08-28 16:54:29 -0600

    added new repo methods

--- microservice/microservices/src/main/java/com/employeesystem/Repository/EmployeeRepository.java
+++ microservice/microservices/src/main/java/com/employeesystem/Repository/EmployeeRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.employeesystem.Repository.VO.EmployeeVO;
 
-public interface EmployeeRepository extends CrudRepository<EmployeeVO, Long>{
+public interface EmployeeRepository extends CrudRepository<EmployeeVO, Long> {
 
-	List<EmployeeVO> findAll(); 
-	
+	List<EmployeeVO> findAll();
+
+	List<EmployeeVO> findByLastnameIgnoreCase(String lastname);
+
+	List<EmployeeVO> findByFirstnameIgnoreCase(String firstname);
+
+	List<EmployeeVO> findByEmailIgnoreCase(String email);
+
 }
